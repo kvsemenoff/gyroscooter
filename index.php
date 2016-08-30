e026eb62641c4b9aa06a2bab062eef22dbfd472a
Konstantin Semenov <kvsemenoff@mail.ru>
2016-08-30 11:19:22 +0400

    firts

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta content="telephone=no" name="format-detection"/>
+
+	<title>Главная страница</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+
+	<?php require_once('includes/header-top.php'); ?>
+	
+	
+</body>
+</html>
+
---
+++ js/common.js
@@ -0,0 +1,128 @@
+
+$(document).ready(function(){
+    $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+    $(".single_image").fancybox();
+    $("a[rel=group]").fancybox({
+      'transitionIn' : 'none',
+      'transitionOut' : 'none',
+      'titlePosition' : 'over',
+      'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+       return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+       }
+    });
+    $("a[rel=group2]").fancybox({
+      'transitionIn' : 'none',
+      'transitionOut' : 'none',
+      'titlePosition' : 'over',
+      'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+       return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+       }
+    });
+
+    $('a[name=modal]').click(function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href');
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+        $('#mask').fadeTo("slow",0.8); 
+        var winH = $(window).height();
+        var winW = $(window).width();
+        posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        $(id).css('top',  posTop+150);
+        $(id).css('left', winW/2-$(id).width()/2);
+        $(id).fadeIn(500); 
+    });
+    $('.window .close').click(function (e) {
+        e.preventDefault();
+        $('#mask, .window').hide();
+        $('.window').hide();
+    }); 
+      
+    $('#mask, .an-exit__krest').click(function () {
+        $('#mask').hide();
+        $('.window').hide();
+    }); 
+
+    $('.mainslider').owlCarousel({
+        loop: true,
+        margin:30,
+        responsiveClass:false,
+        nav: true,
+        items: 5,
+        navText:['<span class="prev arrs"></span>', '<span class="next arrs"></span>'],
+    });
+
+
+   function cleanTnakns(form){
+        $('input[type="text"]').removeClass("error-input");
+        $("input[type=text], textarea").val("");
+        $('.window').hide();
+        $('a[href=#thanks]').trigger('click');
+        
+    }
+
+
+    $('input[type="text"]').mousedown(function() { 
+        $('input[type="text"]').removeClass("error-input");
+    });
+
+    $(".form1").submit(function() { 
+        var tel = $(this).find('input[name="tel"]');
+        var empty = false;
+        if (tel.val() == ""){
+            empty = true;
+        }
+        if (empty == true){
+            tel.addClass("error-input");
+            tel.focus();
+        }else{
+            var form_data = $(this).serialize(); 
+            $.ajax({
+                type: "POST", 
+                url: "/sendmessage.php", 
+                data: form_data,
+                success: function() {
+                    cleanTnakns(this);
+                }
+            });
+        }
+        return false;
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            767:{
+                items:3
+            },
+            850:{
+                items:4
+            },
+            967:{
+                items:5
+            },
+            1067:{
+                items:6
+            },
+            1200:{
+                items:4
+            },
+            1400:{
+                items:5
+            },
+            1600:{
+                items:6
+            }
+        }
+    });
+
+});

a65f70bf281f59a0c9fcb6220c94b6230670d9bf
hatuhov <hatuhov@yandex.ru>
2016-09-06 16:29:29 +0300

    aaaaa

---
+++ popup.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta content="telephone=no" name="format-detection"/>
+
+	<title>Главная страница</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+   
+    <div class="myfirstup">  
+		<span class="myup-span">Купить в 1 клик</span>
+		<span class="myup-span1">Гироскутер модель 1 - <span>16990 р.</span></span>
+		<form action="" class="ah-common-form form1">
+			<span class="ah-exit__krest"></span>
+			<label for="check1" class="">Количество товара</label>
+		    <input type="text" id="check1" class="myinput">
+		    <input type="text" name="uname" class="" placeholder="Ваше имя">
+		    <input type="text" name="tel" class="phone" placeholder="Ваш телефон">
+			<input type="submit" value="Купить в 1 клик" class="">
+		</form>
+	</div>
+
+	<div class="mysecondtup">  
+		<a href="#" class="second_link">«« Вернуться к выбору товаров</a>
+		<span class="second_span">Ваша корзина</span>
+		<div class="tabtitle">
+			<div class="boxleft">
+				<span class="title1">Наименование</span>
+			</div>
+			<div class="boxright">
+				<span class="title2">Цена</span>
+				<span class="title3">Кол-во</span>
+				<span class="title4">Сумма</span>
+				<span class="title5">Удалить</span>
+			</div>
+		</div>
+		
+		<div class="clear"></div>
+
+		<div class="tabrow1">
+			<div class="rcell11">
+				<div class="ah_imgbox"><img src="img/popimg1.png" alt="popimg1"></div>
+				<span class="second_span1">Diamond 1 Hoverbot-k2</span>
+			</div>
+			<div class="rcell12">
+				<span class="second_span2">10 000</span>
+			</div>
+			<div class="rcell13">
+				<a href="#" class="second_link1">-</a>
+				<span class="second_span3">1</span>
+				<a href="#" class="second_link2">+</a>
+			</div>
+			<div class="rcell14">
+				<span class="second_span4">10 000</span>
+			</div>
+			<div class="rcell15">
+				<a href="#" class="second_span5"><img src="img/ah-k.png" alt="ah-k"></a>
+			</div>
+		</div>
+		
+		<div class="clear"></div>
+
+		<div class="tabrow1">
+			<div class="rcell11">
+				<div class="ah_imgbox"><img src="img/popimg2.png" alt="popimg1"></div>
+				<span class="second_span1">Diamond 1 Butterfly Black A1</span>
+			</div>
+			<div class="rcell12">
+				<span class="second_span2">15 000</span>
+			</div>
+			<div class="rcell13">
+				<a href="#" class="second_link1">-</a>
+				<span class="second_span3">2</span>
+				<a href="#" class="second_link2">+</a>
+			</div>
+			<div class="rcell14">
+				<span class="second_span4">30 000</span>
+			</div>
+			<div class="rcell15">
+				<a href="#" class="second_span5"><img src="img/ah-k.png" alt="ah-k"></a>
+			</div>
+		</div>
+
+		<div class="clear"></div>
+
+		<span class="itogspan">Итого:<span> 40 000 р.</span></span>
+		<div class="buttonbox">
+			<a href="#" class="ahbut1">Продолжить покупки</a>
+			<a href="#" class="ahbut2">Купить</a>
+		</div>
+	</div>
+
+</body>
+</html>

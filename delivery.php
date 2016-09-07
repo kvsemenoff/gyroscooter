a1f937a980b20182a4c60491eafeaf631a207ec5
hatuhov <hatuhov@yandex.ru>
2016-09-07 15:20:39 +0300

    sssssssss

---
+++ delivery.php
@@ -0,0 +1,160 @@
+﻿<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta content="telephone=no" name="format-detection"/>
+
+	<title>Доставка и оплата</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/common.js"></script>
+   
+    <?php require_once('includes/header-top.php');?>
+	             
+<section class="ab-content-body">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ab-tovar-item-wrapper">
+					<div class="ab-tovar-item">
+						<div class="ab-img-box">
+							<a href="#"><img src="img/detskie.png" alt=""></a>
+						</div>
+						<span class="ab-box-zagolovok">ДЕТСКИЕ</span>
+						<span class="ab-box-price">От 16990 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/standartnie.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">СТАНДАРТНЫЕ</span>
+						<span class="ab-box-price">От 16990 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/transformers.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">TRANSFORMERS</span>
+						<span class="ab-box-price">От 20990 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/dimond.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">DIAMOND</span>
+						<span class="ab-box-price">От 16990 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/segway.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">SEGWAY</span>
+						<span class="ab-box-price">От 3990 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/solowheel.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">SOLOWHEEL</span>
+						<span class="ab-box-price">От 23950 р.</span>
+					</div>
+					<div class="ab-tovar-item">
+						<div class="ab-img-box"> 
+							<a href="#"><img src="img/drugoe.png" alt=""></a>
+						</div> 
+						<span class="ab-box-zagolovok">ДРУГОЕ</span>
+						<span class="ab-box-price">От 500 р.</span>
+					</div>
+				</div>		
+			</div>       
+		</div>
+	</div>
+</section>
+
+<div class="clearfix"></div>
+
+<section class="del_breadcrumbs">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<ul class="del_breadcrumbs-list">
+					<li class="del_breadcrumbs-item"><a href="#">Главная »</a></li>
+					<li class="del_breadcrumbs-item dbritem">Доставка и оплата</li>
+				</ul>	
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="clearfix"></div>
+
+<section class="delivery_wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="delivery">
+				<div class="delivery_item">
+				<div class="delimgbox">
+					<img src="img/ahdel1.png" alt="ahdel1">
+				</div>
+				<span class="delspan">Закажите товар на сайте или получите консультацию по телефону</span>
+				</div>
+				<div class="delivery_item">
+				<div class="delimgbox">
+					<img src="img/ahdel2.png" alt="ahdel2">
+				</div>
+				<span class="delspan">Мы уже начали проверять Ваш гироскутер</span>
+				</div>
+				<div class="delivery_item">
+				<div class="delimgbox">
+					<img src="img/ahdel3.png" alt="ahdel3">
+				</div>
+				<span class="delspan">Наш курьер уже едет к Вам</span>
+				</div>
+				<div class="delivery_item delitem4">
+				<div class="delimgbox">
+					<img src="img/ahdel4.png" alt="ahdel4">
+				</div>
+				<span class="delspan">Получите гироскутер и проверьте его вместе с курьером еще раз</span>
+				</div>
+				<div class="delivery_item">
+				<div class="delimgbox">
+					<img src="img/ahdel5.png" alt="ahdel5">
+				</div>
+				<span class="delspan">Оплатите заказ наличными либо банковской картой</span>
+				</div>
+				<div class="delivery_item">
+				<div class="delimgbox">
+					<img src="img/ahdel6.png" alt="ahdel6">
+				</div>
+				<span class="delspan">90% заказов мы привозим "день в день"</span>
+				</div>
+				<div class="delivery_item delitem7">
+				<div class="delimgbox">
+					<img src="img/ahdel7.png" alt="ahdel7">
+				</div>
+				<span class="delspan">Наши курьеры покажут вам как кататься, как заряжать и чего делать нельзя</span>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="clearfix"></div>
+
+<?php require_once('includes/footer.php');?>
+
+
